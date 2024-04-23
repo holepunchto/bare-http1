0ea7156c8def79e5f6836ff9f6b17784a7a5a069
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-04-23 10:35:55 +0200

    Add `Server` alias

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-exports.constants = require('./lib/constants')
-
 exports.IncomingMessage = require('./lib/incoming-message')
 exports.OutgoingMessage = require('./lib/outgoing-message')
 
-exports.Server = require('./lib/server')
+const Server = exports.Server = require('./lib/server')
 exports.ServerResponse = require('./lib/server-response')
 
+exports.constants = require('./lib/constants')
+
 exports.createServer = function createServer (onrequest) {
-  return new exports.Server(onrequest)
+  return new Server(onrequest)
 }
