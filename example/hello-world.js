01f47fe1dcc64aaced56ab5493cbee63d2c6ee62
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-05-30 10:05:46 +0200

    Simplify example

--- example/hello-world.js
+++ example/hello-world.js
@@ -1,20 +1,13 @@
 const http = require('..')
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Length', 12)
   res.write('hello world!')
   res.end()
 })
 
-server.on('connection', function (socket) {
-  console.log('got socket')
-  socket.on('close', function () {
-    console.log('closed socket')
-  })
-})
-
-server.listen(8080, function () {
+server.listen(8080, () => {
   console.log(server.address())
 
   const client = http.request({ port: 8080 }, res => {
