12584744016a719471d23e2715a9ceee88a6a62d
yasser <dev@yasser.email>
2024-05-30 04:04:29 -0300

    Add protocol negotiation through the `upgrade` event (#11)
    Co-authored-by: Kasper Isager Dalsgarð <kasperisager@hey.com>

--- test.js
+++ test.js
@@ -384,6 +384,90 @@ test('server and client do big writes', async function (t) {
   server.close()
 })
 
+test('basic protocol negotiation', async function (t) {
+  const up = t.test('upgrade event')
+  up.plan(4)
+
+  const server = http.createServer().listen(0)
+  await waitForServer(server)
+
+  server.on('upgrade', (req, socket, head) => {
+    up.alike(head, Buffer.from('request head'), 'server upgrade event')
+
+    req.on('close', () => up.pass('request closed after server upgrade event'))
+
+    req.on('data', () => t.fail('request data event listener should be detached'))
+    req.on('drain', () => t.fail('request drain event listener should be detached'))
+    req.on('end', () => t.fail('request end event listener should be detached'))
+    req.on('error', () => t.fail('request error event listener should be detached'))
+
+    const handshake = 'HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+      'Upgrade: weird-protocol\r\n' +
+      'Connection: Upgrade\r\n' +
+      '\r\n' +
+      'server head'
+
+    socket.end(handshake)
+  })
+
+  const req = http.request({
+    port: server.address().port,
+    headers: {
+      Connection: 'Upgrade',
+      Upgrade: 'weird-protocol'
+    }
+  }).end('request head')
+
+  req.on('upgrade', (res, socket, head) => {
+    up.alike(head, Buffer.from('server head'), 'request upgrade event')
+
+    req.on('close', () => up.pass('request closed after request upgrade event'))
+
+    res.on('close', () => t.fail('response close event listener should be detached'))
+    res.on('data', () => t.fail('response data event listener should be detached'))
+    res.on('drain', () => t.fail('response drain event listener should be detached'))
+    res.on('end', () => t.fail('response end event listener should be detached'))
+    res.on('error', () => t.fail('response error event listener should be detached'))
+
+    socket.end()
+  })
+
+  await up
+
+  server.close()
+})
+
+test('close connection if missing upgrade handler', async function (t) {
+  const ce = t.test('close event')
+  ce.plan(1)
+
+  const server = http.createServer().listen(0)
+  await waitForServer(server)
+
+  server.on('upgrade', (req, socket, head) => {
+    const handshake = 'HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+      'Upgrade: weird-protocol\r\n' +
+      'Connection: Upgrade\r\n' +
+      '\r\n'
+
+    socket.end(handshake)
+  })
+
+  const req = http.request({
+    port: server.address().port,
+    headers: {
+      Connection: 'Upgrade',
+      Upgrade: 'weird-protocol'
+    }
+  }).end()
+
+  req.on('close', () => ce.pass('connection closed'))
+
+  await ce
+
+  server.close()
+})
+
 test('make requests using url', async function (t) {
   const rqts = t.test('requests')
   t.plan(2)
